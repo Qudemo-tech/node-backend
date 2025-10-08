81da58955ad317dec4ee3c73e4e20c8dd4116c88
AbhishekRavi063 <abhishekravi063@gmail.com>
2025-10-08 19:10:21 +0530

    fixx v2

--- migrations/add_original_filename_column.sql
+++
@@ -1,8 +0,0 @@
--- Add original_filename column to qudemo_shares table
--- This column will store the original filename uploaded by users for bulk uploads
-
-ALTER TABLE qudemo_shares 
-ADD COLUMN IF NOT EXISTS original_filename TEXT;
-
--- Add a comment to document the column purpose
-COMMENT ON COLUMN qudemo_shares.original_filename IS 'Original filename of the uploaded file for bulk uploads';
